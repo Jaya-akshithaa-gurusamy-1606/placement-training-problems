00719ffc61f1f1b7cf5046ac8749d2c2340c5b53
Jaya-akshithaa-gurusamy-1606 <akshiguru1606@gmail.com>
2025-10-07 23:12:21 +0530

    Create reversed number (with destroying the variable).java
    Enter a number: 1432
    Original number: 1432
    Reversed number: 2341

---
+++ reversed number (with destroying the variable).java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt(); 
+        int rev = 0;
+        int b = 1;  
+        while (b <= num) {
+            int rem = (num / b) % 10;
+            rev = rev * 10 + rem;
+            b *= 10;
+        }
+        System.out.println("Original number: " + num);
+        System.out.println("Reversed number: " + rev);
+    }
+}
