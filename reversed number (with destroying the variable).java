d18a5d6c8312f6a4d9f27f6a77fe9ee8102e263c
Jaya-akshithaa-gurusamy-1606 <akshiguru1606@gmail.com>
2025-10-07 23:15:47 +0530

    Update reversed number (with destroying the variable).java
    Enter a number: 1234
    Original number: 1234
    Reversed number: 4321

--- reversed number (with destroying the variable).java
+++ reversed number (with destroying the variable).java
@@ -3,14 +3,15 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int num = sc.nextInt(); 
+        int num = sc.nextInt(); // original number stays intact
         int rev = 0;
-        int b = 1;  
-        while (b <= num) {
-            int rem = (num / b) % 10;
-            rev = rev * 10 + rem;
-            b *= 10;
+        int temp = num; // used only for calculation
+        while (temp != 0) {
+            int rem = temp % 10;  // get last digit
+            rev = rev * 10 + rem; // build reversed number
+            temp /= 10;            // remove last digit
         }
+
         System.out.println("Original number: " + num);
         System.out.println("Reversed number: " + rev);
     }
