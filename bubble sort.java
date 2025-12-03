70d943b1c26fc6ebbe8e20e8c27d3d358876f9df
Jaya-akshithaa-gurusamy-1606 <akshiguru1606@gmail.com>
2025-12-03 09:59:59 +0530

    Create bubble sort.java
    Enter the size of array: 5
    Enter the values of array: 7 3 9 1 5
    Array sorted: 
    1 3 5 7 9

---
+++ bubble sort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+        System.out.print("Enter the values of array: ");
+        int[] a = new int[n];
+        for(int i = 0;i<n;i++){
+            a[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            int flag = 0;
+            for(int j=0;j<n-1-i;j++){
+                if(a[j] > a[j+1]){
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                    flag = 1;
+                }
+            }
+            if(flag == 0) break;
+        }
+        System.out.println("Array sorted: ");
+        for(int i=0;i<n;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
