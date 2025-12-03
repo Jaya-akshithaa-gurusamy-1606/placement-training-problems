917a532edb65b16c992e1398ed32cf76c887e549
Jaya-akshithaa-gurusamy-1606 <akshiguru1606@gmail.com>
2025-12-03 11:28:48 +0530

    Create star pattern - M.java
    Enter a number : 5
    *           * 
    *  *    *  * 
    *     *     * 
    *            * 
    *            *

---
+++ star pattern - M.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s = new Scanner(System.in);
+		System.out.print("Enter a number : ");
+		int n = s.nextInt();
+		int st = n ;
+		for(int i=1;i<=n;i++){
+		    for(int j=1;j<=st;j++){
+		        if((i+j==n+1&&i<=j)||(i==j && i<=((n+1)/2))||j==1||j==st)
+		            System.out.print("* ");
+		        else 
+		            System.out.print("  ");
+		    }
+		    System.out.println();
+		}
+	}
+}
