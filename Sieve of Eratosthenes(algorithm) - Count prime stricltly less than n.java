0ff2c12da5307f9961556e85b0a2befe3789aba6
Jaya Akshithaa G <akshiguru1606@gmail.com>
2025-12-13 09:45:25 +0530

    Create Sieve of Eratosthenes(algorithm) - Count prime stricltly less than n.java
    Enter the value: 20
    Number of primes less than 20 is: 8

---
+++ Sieve of Eratosthenes(algorithm) - Count prime stricltly less than n.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Main {
+    public int countPrimes(int n) {
+        if (n <= 2) return 0;
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) count++;
+        }
+
+        return count;
+    }
+    public static void main(String[] args) {
+        Main sol = new Main();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value: ");
+        int n = sc.nextInt();
+        int result = sol.countPrimes(n);
+
+        System.out.println("Number of primes less than " + n + " is: " + result);
+    }
+}
