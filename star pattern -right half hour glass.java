abf050c6799eb6afcb03e56e023f32fe45cb1120
Jaya Akshithaa G <akshiguru1606@gmail.com>
2026-01-21 12:24:01 +0530

    Create star pattern -right half hour glass.java
    Enter the size : 5
    *****
     ****
      ***
       **
        *
       **
      ***
     ****
    *****

---
+++ star pattern -right half hour glass.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main{
+public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    System.out.print("Enter the size : ");
+    int n = s.nextInt();
+    int sp = 0;
+    int st = n;
+    for(int i = 0;i<(2*n-1);i++){
+        for(int j=0;j<sp;j++){
+          System.out.print(" ");
+        }   
+        for(int j=0;j<st;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+        if(i<n-1){
+            sp++;
+            st--;
+        }
+        else{
+            st++;
+            sp--;
+        }
+    }
+}}
