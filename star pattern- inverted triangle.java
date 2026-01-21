393e586a32f71571bfa30b1bdc017804e1a7f7df
Jaya Akshithaa G <akshiguru1606@gmail.com>
2026-01-21 12:26:12 +0530

    Create star pattern- inverted triangle.java
    Enter the size : 5
    *********
     *******
      *****
       ***
        *

---
+++ star pattern- inverted triangle.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main{
+public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    System.out.print("Enter the size : ");
+    int n = s.nextInt();
+    int sp = 0;
+    int st = (2*n)-1;
+    for(int i = 0;i<n;i++){
+        for(int j=0;j<sp;j++){
+           System.out.print(" ");
+        }   
+        for(int j=0;j<st;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+        st-=2;
+        sp++;
+    }
+}}
