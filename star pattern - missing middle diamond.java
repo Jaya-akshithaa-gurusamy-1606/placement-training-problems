86954861b3bc13dba7ee481ea940f533f34bd3ea
Jaya Akshithaa G <akshiguru1606@gmail.com>
2026-01-21 11:41:53 +0530

    Create star pattern - missing middle diamond.java
    Enter the size : 5
    **********
    ****   ****
    ***      ***
    **         **
    *            *
    **         **
    ***      ***
    ****   ****
    **********

---
+++ star pattern - missing middle diamond.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the size : ");
+        int n = s.nextInt();
+        int sp = 0;
+        int st = n;
+        for(int i = 0;i<2*n-1;i++){
+            for(int j=0;j<st;j++){
+              System.out.print("*");
+            }   
+            for(int j=0;j<sp;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<st;j++){
+              System.out.print("*");
+            }
+            System.out.println();
+            if(i>n-2){
+                sp-=2;
+                st++;
+            }
+            else{
+                sp+=2;
+                st--;
+            }
+        }
+    }
+}
